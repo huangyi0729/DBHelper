999b65279c958d03e027ac4d8beb5eb224db1020
huangy0729 <3141397082@qq.com>
2026-05-27 08:39:13 +0800

    one new file

---
+++ 卷积神经网络/lianxi.py
@@ -0,0 +1,10 @@
+import numpy as np
+# list_1d=[1,2,3,4,5]
+# arr_1d=np.array(list_1d)
+# print(arr_1d)
+# list_2d=[[1,2,3],[4,5,6],[7,8,9]]
+# arr_2d=np.array(list_2d)
+# print(arr_2d)
+list_3d=[[[1,2,3],[4,5,6],[7,8,9]],[[9,8,7],[6,5,4],[3,2,1]],[[7,8,6],[5,6,3],[1,7,4]]]
+arr_3d=np.array(list_3d)
+print(arr_3d)
